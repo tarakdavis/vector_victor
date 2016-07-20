d84fadce2263ab3cd0f191585d98698d3a51a223
Tara Davis <tkalindavis@gmail.com>
2016-07-20 16:41:46 -0400

    Created compare_shapes function and dot function passes test

--- linear_algebra.py
+++ linear_algebra.py
@@ -1,23 +1,39 @@
-import math
+# import math
 
 
 def shape(vector):
     return (len(vector), )
 
 
+def compare_shapes(*args):
+    return len(set([shape(item) for item in args])) == 1
+
+
 def vector_add(a, b):
-    if shape(a) != shape(b):
+    if not compare_shapes(a, b):
         raise ShapeError
     else:
         return [sum(values) for values in zip(a, b)]
 
+
 def vector_sub(a, b):
-    if shape(a) != shape(b):
+    if not compare_shapes(a, b):
         raise ShapeError
     else:
         return [x - y for x, y in zip(a, b)]
 
 
+def vector_sum(*args):
+    if not compare_shapes(*args):
+        raise ShapeError
+    return [sum(values) for values in zip(*args)]
+
+
+def dot(a, b):
+    if not compare_shapes(a, b):
+        raise ShapeError
+    else:
+        return sum([x * y for x, y in zip(a, b)])
 
 
 class ShapeError(Exception):
