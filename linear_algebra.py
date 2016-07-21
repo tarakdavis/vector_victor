7fb6d02d506f99beffdfb2c2f8c29730651d0042
Tara Davis <tkalindavis@gmail.com>
2016-07-21 09:54:50 -0400

    Cleaned up code/repition.

--- linear_algebra.py
+++ linear_algebra.py
@@ -6,34 +6,28 @@ def shape(vector):
 
 
 def compare_shapes(*args):
-    return len(set([shape(item) for item in args])) == 1
+    if not len({shape(item) for item in args}) == 1 == 1:
+        raise ShapeError
 
 
 def vector_add(a, b):
-    if not compare_shapes(a, b):
-        raise ShapeError
-    else:
-        return [sum(values) for values in zip(a, b)]
+    compare_shapes(a, b)
+    return [sum(values) for values in zip(a, b)]
 
 
 def vector_sub(a, b):
-    if not compare_shapes(a, b):
-        raise ShapeError
-    else:
-        return [x - y for x, y in zip(a, b)]
+    compare_shapes(a, b)
+    return [x - y for x, y in zip(a, b)]
 
 
 def vector_sum(*args):
-    if not compare_shapes(*args):
-        raise ShapeError
+    compare_shapes(*args)
     return [sum(values) for values in zip(*args)]
 
 
 def dot(a, b):
-    if not compare_shapes(a, b):
-        raise ShapeError
-    else:
-        return sum([x * y for x, y in zip(a, b)])
+    compare_shapes(a, b)
+    return sum([x * y for x, y in zip(a, b)])
 
 
 def vector_multiply(vector, scalar):
